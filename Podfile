82b7b3f0deecbe691fbfe7e03da22b67381090d3
zerdzhong <zerd.zhong@gmail.com>
2014-12-04 23:58:17 +0800

    add cocoapods

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '8.0'
+pod 'PinYin4Objc', '~> 1.1.1'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// PinYin4Objc
+#define COCOAPODS_POD_AVAILABLE_PinYin4Objc
+#define COCOAPODS_VERSION_MAJOR_PinYin4Objc 1
+#define COCOAPODS_VERSION_MINOR_PinYin4Objc 1
+#define COCOAPODS_VERSION_PATCH_PinYin4Objc 1
+
